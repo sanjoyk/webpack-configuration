5ec7a94b869c23c4bdd31c38f3544b77456e4c57
sanjoy sardar <sksardar42@gmail.com>
2017-07-22 09:49:26 +0530

    added es6 to es5 conversion configuration

---
+++ configurations/es6-to-es5/webpack.config.js
@@ -0,0 +1,19 @@
+var webpack = require("webpack");
+var path = require("path");
+module.exports ={
+	entry: "index.js",
+	output :{
+		path : path.resolve(__dirname, "dest"),
+		filename : "index.js"	
+	},
+	module : {
+		loaders : [
+			{
+				test : /\.js$/,
+				exclude :/node_modules/,
+				loader : "babel-loader"	
+			}
+		]
+	}
+}
+
